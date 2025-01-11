6ca647d3df808a12558c588b64c4eeca1c29f552
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-01-11 23:49:55 +0600

    Added solved problems for 2025-01-11

---
+++ problem_01.js
@@ -0,0 +1,34 @@
+let secondInteger = "4";
+let secondDecimal = "4.0";
+let secondString = "love yourself";
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    
+    const x = Number(secondInteger);
+    
+    console.log(x+firstInteger)
+    
+    
+    const y = parseFloat(secondDecimal);
+    
+    console.log(y+firstDecimal);
+    
+    const z = firstString + secondString;
+    console.log(z);
+    
+    
+    
+    
+    
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    
+}
