efc09e390cfc7f39d0acbd5ff594ccba1afb6ca3
mushfiq7297 <mushfiqurrahman7297@gmail.com>
2025-01-19 16:07:38 +0600

    reversed string problem solved

---
+++ prbl_07.js
@@ -0,0 +1,21 @@
+
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    
+    try{
+        const reversed = s.split('').reverse().join('');
+        console.log(reversed);
+    }
+    catch(error)
+    {
+        console.log(error.message)
+        console.log(s)
+    }
+}
+   
+const s = "1234";
+    reverseString(s)
